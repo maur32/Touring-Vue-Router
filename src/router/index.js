be19ce33efbd074ecb5db3dfb3053b698bfc15cd
maur32 <maur.martins32@gmail.com>
2023-01-05 15:54:31 -0300

    Criando rotas aninhadas

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '../views/EventList.vue'
-import EventDetails from '../views/EventDetails.vue'
+import EventLayout from '../views/event/Layout.vue'
+import EventDetails from '../views/event/Details.vue'
+import EventRegister from '../views/event/Register.vue'
+import EventEdit from '../views/event/Edit.vue'
 import About from '../views/About.vue'
 
 const routes = [
@@ -16,6 +19,30 @@ const routes = [
     props: true,
     component: EventDetails
   },
+  {
+    path: '/event/:id',
+    name: 'EventLayout',
+    props: true,
+    component: EventLayout,
+    children: [
+      {
+        path: '',
+        name: 'EventDetails',
+        component: EventDetails
+      },
+      {
+        path: 'register',
+        name: 'EventRegister',
+        component: EventRegister
+      },
+      {
+        path: 'edit',
+        name: 'EventEdit',
+        component: EventEdit
+      }
+    ]
+  },
+
   {
     path: '/about',
     name: 'About',
